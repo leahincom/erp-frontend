5ecce5ee0f6d52569632c63111d76c8ea4077bfb
JungHyun <leahincom@gmail.com>
2021-08-24 21:40:49 +0900

    feat: UserContext Provider 설정

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, useReducer, useEffect } from 'react';
+
+export const UserStateContext = createContext({});
+export const UserDispatchContext = createContext({});
+
+type STATETYPE = {
+  isAuth: boolean;
+};
+
+type ACTIONTYPE = { type: 'LOGIN' } | { type: 'LOGOUT' };
+
+const reducer = (state: STATETYPE, action: ACTIONTYPE): STATETYPE => {
+  switch (action.type) {
+    case 'LOGIN': {
+      return {
+        isAuth: true,
+      };
+    }
+    case 'LOGOUT': {
+      return {
+        isAuth: false,
+      };
+    }
+    default: {
+      throw new Error('Unhandled action type');
+    }
+  }
+};
+
+export interface UserProviderProps {
+  children: Element;
+  isAuthenticated: boolean;
+}
+
+const UserProvider = ({ children, isAuthenticated }: UserProviderProps) => {
+  const [state, dispatch] = useReducer(reducer, { isAuth: isAuthenticated });
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch({ type: 'LOGIN' });
+    } else {
+      dispatch({ type: 'LOGOUT' });
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <UserStateContext.Provider value={state}>
+      <UserDispatchContext.Provider value={dispatch}>{children}</UserDispatchContext.Provider>
+    </UserStateContext.Provider>
+  );
+};
+
+export default UserProvider;
