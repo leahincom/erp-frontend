ec04b90e293c8f55745319b1a007aafcb8a89b83
JungHyun <leahincom@gmail.com>
2021-08-25 23:47:15 +0900

    feat: utils 세팅 및 구현

---
+++ src/utils/setCaretToEnd.tsx
@@ -0,0 +1,11 @@
+const setCaretToEnd = (element: HTMLElement) => {
+  const range = document.createRange();
+  const selection = window.getSelection();
+  range.selectNodeContents(element);
+  range.collapse(false);
+  selection?.removeAllRanges();
+  selection?.addRange(range);
+  element.focus();
+};
+
+export default setCaretToEnd;
