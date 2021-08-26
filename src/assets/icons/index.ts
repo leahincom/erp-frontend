cd5afec34082f5c33c64775518125f84266f7d11
JungHyun <leahincom@gmail.com>
2021-08-26 23:34:17 +0900

    chore: assets public으로 이동

--- src/assets/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as CloseIcon } from './Close.svg';
-export { default as UserIcon } from './User.svg';
-export { default as GithubIcon } from './Github.svg';
-export { default as DraggableIcon } from './Draggable.svg';
-export { default as MoreIcon } from './More.svg';
-export { default as TrashIcon } from './Trash.svg';
