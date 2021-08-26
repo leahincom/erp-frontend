09e4a3afab71857197952701a331ccf6049307e3
JungHyun <leahincom@gmail.com>
2021-08-26 23:35:50 +0900

    feat: objectId 생성 utils 구현

---
+++ src/utils/objectId.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const objectId = () => {
+  const timestamp = ((new Date().getTime() / 1000) | 0).toString(16);
+  return (
+    timestamp +
+    'xxxxxxxxxxxxxxxx'
+      .replace(/[x]/g, function () {
+        return ((Math.random() * 16) | 0).toString(16);
+      })
+      .toLowerCase()
+  );
+};
+
+export default objectId;
