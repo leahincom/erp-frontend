48f10dae28415d138f777170ac0e8bc7f6d4892f
JungHyun <leahincom@gmail.com>
2021-08-26 23:41:03 +0900

    feat: pages/[pid] 페이지 구현

---
+++ src/pages/p/[pid].tsx
@@ -0,0 +1,36 @@
+import { NextPageContext } from 'next';
+import React from 'react';
+import { resetServerContext } from 'react-beautiful-dnd';
+
+import { PageProps } from '..';
+import EditablePage from '../../components/EditablePage';
+
+const Page = ({ pid, blocks, err }: PageProps) => {
+  return <EditablePage id={pid} fetchedBlocks={blocks} err={err} />;
+};
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  resetServerContext();
+
+  const pageId = context.query.pid;
+  const req = context.req;
+
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  req && req.headers && req.headers.cookie && headers.append('Cookie', req.headers.cookie);
+
+  try {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API}/pages/${pageId}`, {
+      method: 'GET',
+      credentials: 'include',
+      // Forward the authentication cookie to the backend
+      headers,
+    }).then((res) => res.json());
+    return { props: { blocks: data.page.blocks, pid: pageId, err: false } };
+  } catch (err) {
+    console.log(err);
+    return { props: { blocks: null, pid: null, err: true } };
+  }
+};
+
+export default Page;
