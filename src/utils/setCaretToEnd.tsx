5bba2500aeb1ee9f3f3b97aabcc1473ab74323f9
JungHyun <leahincom@gmail.com>
2021-08-26 23:35:37 +0900

    chore: setCaretToEnd types 수정

--- src/utils/setCaretToEnd.tsx
+++ src/utils/setCaretToEnd.tsx
@@ -1,11 +1,11 @@
-const setCaretToEnd = (element: HTMLElement) => {
+const setCaretToEnd = (element: Element) => {
   const range = document.createRange();
   const selection = window.getSelection();
   range.selectNodeContents(element);
   range.collapse(false);
   selection?.removeAllRanges();
   selection?.addRange(range);
-  element.focus();
+  (element as HTMLElement).focus();
 };
 
 export default setCaretToEnd;
