e3e7ac316d088c822c9f87399f0a77b78c95a55c
JungHyun <leahincom@gmail.com>
2021-12-06 16:40:21 +0900

    [FIX] getSelection null 조건 설정

--- src/utils/getSelection.tsx
+++ src/utils/getSelection.tsx
@@ -1,16 +1,10 @@
 const getSelection = (element: HTMLElement) => {
-  let selectionStart: number | undefined;
-  let selectionEnd: number | undefined;
-  const isSupported = typeof window.getSelection !== 'undefined';
-
-  if (isSupported) {
-    const range = window.getSelection().getRangeAt(0);
-    const preSelectionRange = range.cloneRange();
-    preSelectionRange.selectNodeContents(element);
-    preSelectionRange.setEnd(range.startContainer, range.startOffset);
-    selectionStart = preSelectionRange.toString().length;
-    selectionEnd = selectionStart + range.toString().length;
-  }
+  const range = window.getSelection()?.getRangeAt(0);
+  const preSelectionRange = range?.cloneRange();
+  preSelectionRange?.selectNodeContents(element);
+  range && preSelectionRange?.setEnd(range?.startContainer, range?.startOffset);
+  const selectionStart = preSelectionRange?.toString().length;
+  const selectionEnd = selectionStart && range && selectionStart + range?.toString().length;
 
   return { selectionStart, selectionEnd };
 };
